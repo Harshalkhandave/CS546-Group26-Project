0943d508d942a7568a8cc00e3eb803bf2db8eb35
Harshal Khandave <harshalkhandave@mac.mynetworksettings.com>
2025-12-03 13:04:35 -0500

    Setup DB

--- config/mongoConnection.js
+++ config/mongoConnection.js
@@ -1,17 +1,16 @@
-import {MongoClient} from 'mongodb';
-import {mongoConfig} from './settings.js';
+import mongoose from "mongoose";
 
-let _connection = undefined;
-let _db = undefined;
-
-export const dbConnection = async () => {
-  if (!_connection) {
-    _connection = await MongoClient.connect(mongoConfig.serverUrl);
-    _db = _connection.db(mongoConfig.database);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(
+        "mongodb+srv://harshalkhandave19_db_user:NdlOn3UpsnzGKKpW@cluster0.izltwpm.mongodb.net/?appName=Cluster0",
+      {}
+    );
+    console.log("✅ MongoDB connected!");
+  } catch (error) {
+    console.error("❌ MongoDB connection failed:", error.message);
+    process.exit(1);
   }
+};
 
-  return _db;
-};
-export const closeConnection = async () => {
-  await _connection.close();
-};
+export default connectDB;
