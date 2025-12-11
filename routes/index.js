d56dc9a288ab31ffd6011ed35b610222eb7e6712
jwang266 <jingtongwang68@gmail.com>
2025-12-10 23:51:09 -0500

    add middleware.js and update redirect paths to /users

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ import usersRoutes from './users.js';
 import boroughsRoutes from './boroughs.js';
 
 const constructorMethod = (app) => {
-  app.use('/', homeRoutes);
-  app.use('/user', usersRoutes);
+  app.use('/users', usersRoutes);
   app.use('/boroughs', boroughsRoutes);
+  app.use('/', homeRoutes);
 
   app.use('{*splat}', (req, res) => {
     res.status(404).json({error: 'Route Not found'});
